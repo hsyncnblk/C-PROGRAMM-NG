5a810c2dd404d872626d26aa2a4d9441915f28fa
HÜSEYİN ÇİNİBULAK <72135424+hsyncnblk@users.noreply.github.com>
2021-05-22 12:16:20 +0300

    Create ELEKTRONİK ÜRÜN VERGİSİ.C

---
+++ ELEKTRONİK ÜRÜN VERGİSİ.C
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    int fiyat;
+    float sfiyat;
+    int secim;
+
+    printf("********************OYUN KONSOLU , TELEFON VEYA LAPTOP VERGISI HESAPLAMA ARACI********************\n\n");
+    
+    printf("hangi urunun vergisini hesaplamak istiyorsan yanindaki numarayi gir \n\n");
+    printf("1-)telefon\n2-)oyun konsolu\n3-)laptop \n\n");
+   
+    printf("Secim:");
+    scanf("%d",&secim);
+
+    switch(secim)
+    {
+
+    case 1:
+        printf("telefonun vergisiz fiyti nedir : ");
+        scanf("%d",&fiyat);
+        sfiyat=(fiyat*(0.5))+(fiyat*(0.18))+(fiyat*(0.1))+(fiyat*(0.1))+fiyat;
+        printf("\ntelefonun vergili fiyati: %.3f \n",sfiyat);
+        break;
+
+    case 2:
+        printf("oyun konsolunun vergisiz fiyti nedir : ");
+        scanf("%d",&fiyat);
+        sfiyat==(fiyat*(0.2))+(fiyat*(0.24))+(fiyat*(0.25))+fiyat;
+        printf("\noyun konsolunun vergili fiyati: %.3f \n",sfiyat);
+        break;
+
+    case 3:
+        printf("leptop'un vergisiz fiyti nedir : ");
+        scanf("%d",&fiyat);
+        sfiyat=fiyat*(0.18)+fiyat;
+        printf("\leptop'un vergili fiyati: %.3f \n",sfiyat );
+        break;
+
+    default:
+        printf("\nHATALI TUSA BASTINIZ \n");
+
+    }
+
+    return 0;
+}
